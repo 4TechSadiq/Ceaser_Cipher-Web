c0ea0d0686a4c9a72a5bf0d380257366cf7fd523
Mohamed Rizad <rizadvillan@gmail.com>
2023-10-09 19:01:02 +0530

    fixx app 700

--- app.py
+++ app.py
@@ -25,3 +25,7 @@ def home():
 
 
 app.run(host="0.0.0.0", port=80, use_reloader=True, threaded=True,debug=True)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="0.0.0.0", port=80, use_reloader=True, threaded=True,debug=False)
